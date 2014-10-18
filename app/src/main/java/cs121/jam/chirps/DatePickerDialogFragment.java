c7607693a8232db928af5deefc0db1bcc74da6a1
MaiHo <maingocho94@gmail.com>
2014-10-17 21:16:45 -0700

    Added inline data validation for chirp submission.

--- app/src/main/java/cs121/jam/chirps/DatePickerDialogFragment.java
+++ app/src/main/java/cs121/jam/chirps/DatePickerDialogFragment.java
@@ -1,7 +1,34 @@
 package cs121.jam.chirps;
 
-/**
- * Created by maiho on 10/17/14.
- */
-public class DatePickerDialogFragment {
+import java.util.Calendar;
+import java.util.Date;
+
+import android.app.DialogFragment;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.Dialog;
+import android.os.Bundle;
+
+public class DatePickerDialogFragment extends DialogFragment {
+
+    private OnDateSetListener mDateSetListener;
+
+    public DatePickerDialogFragment() {
+        // nothing to see here, move along
+    }
+
+    public DatePickerDialogFragment(OnDateSetListener callback) {
+        mDateSetListener = (OnDateSetListener) callback;
+    }
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 7);
+
+        // Default date is 7 days from the current day.
+        return new DatePickerDialog(getActivity(),
+                mDateSetListener, cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+    }
+
 }
--- app/src/main/java/cs121/jam/chirps/TimePickerDialogFragment.java
+++ app/src/main/java/cs121/jam/chirps/TimePickerDialogFragment.java
@@ -1,7 +1,34 @@
 package cs121.jam.chirps;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+
+import java.util.Calendar;
+
 /**
  * Created by maiho on 10/17/14.
  */
-public class TimePickerDialogFragment {
+public class TimePickerDialogFragment extends DialogFragment {
+
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener;
+
+    public TimePickerDialogFragment() {
+        // nothing to see here, move along
+    }
+
+    public TimePickerDialogFragment(TimePickerDialog.OnTimeSetListener callback) {
+        mTimeSetListener = (TimePickerDialog.OnTimeSetListener) callback;
+    }
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Calendar cal = Calendar.getInstance();
+
+        return new TimePickerDialog(getActivity(),
+                mTimeSetListener, cal.get(Calendar.HOUR),
+                cal.get(Calendar.MINUTE),
+                DateFormat.is24HourFormat(getActivity()));
+    }
 }
